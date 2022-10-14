84e2bb3551842957d01d7e2dc4d019bb8306e434
sad <cute_gopher@outlook.com>
2022-10-14 16:20:19 +0800

    init

---
+++ src/main/java/com/tenda/middleware/middlewaretutorial/MiddlewareTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.tenda.middleware.middlewaretutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewareTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddlewareTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenda/middleware/middlewaretutorial/MiddlewareTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenda.middleware.middlewaretutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddlewareTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
